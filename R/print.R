ac5ab1be3e14c9a0161db2b84ef16eea5a323179
JosepER <jespasareig@gmail.com>
2023-09-05 18:39:32 +0200

    modify print_indicator and lower-level functions

--- R/print.R
+++ R/print.R
@@ -41,22 +41,31 @@ print_percentiles <- function(lissy_files, variable, breaks = seq(0, 1, 0.1), we
 #'
 #' @param lissy_files A list of LIS or LWS files.
 #' @param variable A character string indicating the aggregate for which the indicator needs to be computed.
+#' @param weight A string with the name of the variable in 'file' that should be used as sample weights.
 #' @param na.rm A boolean. Indicates if NAs should be ignored. Defaults to FALSE.
+#' @param files_level  A string indicating the level of the file. Valid inputs are:
+#'   'household', 'h', 'person' or 'p'. If NULL (default), the file level will
+#'   be retrived from the 'lissy_files' attributes.
+#' @param variable_level Level of the variable. Should be either 'household', 'h', 'person' or 'p'.
+#'   If NULL (default), the function will try to guess the level of the variable.
+#'   This is done by comparing the value in 'variable' with pre-set lists of variables.
 #' @return A named numeric vector with the Gini coefficients.
 #' @examples
 #' \dontrun{
 #' lissy_datasets <- read_lissy_files(c("fr84h", "fr94h", "fr10h"))
 #' print_gini(lissy_datasets = lissy_datasets, variable = "dhi", na.rm = TRUE)
 #' }
-print_gini <- function(lissy_files, variable, na.rm = FALSE){
+print_gini <- function(lissy_files, variable, weight = NULL, na.rm = FALSE, files_level = NULL, variable_level = NULL){
 
   lissy_files %>%
     purrr::imap_dbl(
       ~compute_gini(file = ..1,
                     file_name = ..2,
                     variable = ..3,
-                    na.rm = ..4),
+                    weight = ..4,
+                    na.rm = ..5),
     variable,
+    weight,
     na.rm)
 
 }
@@ -93,66 +102,44 @@ print_all_lissy_files <- function(database){
 #' @param variable A character string indicating the aggregate for which the indicator needs to be computed.
 #' @param indicator A character string indicating the type of indicator statistic to be computed.
 #'   Currently the function supports only 'mean', 'median', 'ratio' and 'gini'.
-#' @param files_level  A string indicating the level of the file. Valid inputs are:
-#'   'household', 'h', 'person' or 'p'. If NULL (default), the file level will
-#'   be retrived from the 'lissy_files' attributes.
-#' @param variable_level Level of the variable. Should be either 'household', 'h', 'person' or 'p'.
-#'   If NULL (default), the function will try to guess the level of the variable.
-#'   This is done by comparing the value in 'variable' with pre-set lists of variables.
 #' @param weight A string with the name of the variable in 'file' that should be
-#'   used as sample weights.
+#'   used as sample weights. If NULL (default), the function tries to guess the
+#'   needed weight to compute the indicator. This guess is made on the information
+#'   from files_level and variable_level.
 #' @param ratio A vector of two numeric values between 0 and 1.Only used in the computation of 'ratio' indicator.
 #'   Defines the percentiles in the numerator and denominator respectively.
 #'   E.g. (0.9, 0.1) computes the 90/10 ratio.
 #' @param epsilon A numeric vector of length one. Only used in the computation of 'atkinson' indicator'.
 #'   The inequality adversion parameter. Needs to be epsilon > 0.
 #' @param na.rm A boolean. Indicates if NAs should be ignored. Defaults to FALSE.
+#' @param files_level  A string indicating the level of the file. Valid inputs are:
+#'   'household', 'h', 'person' or 'p'. If NULL (default), the file level will
+#'   be retrived from the 'lissy_files' attributes.
+#' @param variable_level Level of the variable. Should be either 'household', 'h', 'person' or 'p'.
+#'   If NULL (default), the function will try to guess the level of the variable.
+#'   This is done by comparing the value in 'variable' with pre-set lists of variables.
 #'
 #' @return A numeric vector.
-print_indicator <- function(lissy_files, variable, indicator, files_level = NULL, variable_level = NULL, weight = NULL, ratio = NULL, epsilon = NULL, na.rm = FALSE){
+print_indicator <- function(lissy_files, variable, indicator, weight = NULL, ratio = NULL, epsilon = NULL, na.rm = FALSE, files_level = NULL, variable_level = NULL){
 
   assertthat::assert_that(indicator %in% c("mean", "median", "ratio", "gini", "atkinson"),
                           msg = "Currently supported indicators are 'mean', 'median', 'ratio', 'atkinson' and 'gini'.")
 
 
-  if(missing(files_level)){
+  if(missing(weight)){
 
-    level_ <- get_lissy_attributes(lissy_files)[["level"]]
+    weight_var <- determine_weight(lissy_files, variable, files_level, variable_level)
+    message(glue::glue("{weight_var} will be used as weighting variable."))
 
-  }else{
-
-    level_ <- files_level
-
-  }
-
-
-  if(is.null(variable_level)){
-
-    variable_level <- check_variable_level(variable)
-
-  }else{
-
-    assertthat::assert_that(variable_level %in% c("person", "household", "p", "h"),
-                            msg = "Argument 'variable_level' can only take 'person', 'p', 'household' or 'h' as values.")
-  }
-
-
-  if(is.null(weight) & variable_level %in% c("person", "p")){
-
-    weight_var <- "pwgt"
-
-  }else if(is.null(weight) & variable_level %in% c("household", "h")){
-
-    weight_var <- "hwgt"
-
-  }else{
-
+  } else {
     weight_var <- weight
-
   }
+
+  level_ <- determine_file_level(lissy_files, files_level)
+
 
   # for hh-level files, multiply the variable by 'nhhmem'
-  if(level_ %in% c("household", "h")){
+  if(!is.null(level_) && level_ %in% c("household", "h")){
 
     assertthat::assert_that(purrr::every(lissy_files,
                                          ~"nhhmem" %in% names(.x) ),
@@ -229,14 +216,82 @@ print_indicator <- function(lissy_files, variable, indicator, files_level = NULL
                     .f = ~compute_atkinson(file = ..1, # .x
                                            file_name = ..2, # .y
                                            variable = ..3,
-                                           weight = ..4,
-                                           epsilon = ..5,
+                                           epsilon = ..4,
+                                           weight = ..5,
                                            na.rm = ..6),
                     variable, # ..3
-                    weight_var, # ..4
-                    epsilon, # ..5
+                    epsilon, # ..4
+                    weight_var, # ..5
                     na.rm # ..6
     )
   }
 }
 
+
+#' Determine the name of the weight variable
+#'
+#' Computes the name of the weight variable based on the information from the 'lissy_files'
+#'   object, 'files_level' and 'variable_level'
+#'
+#' @param lissy_files A list of LIS or LWS files.
+#' @param variable A character string indicating the aggregate for which the indicator needs to be computed.
+#' @param files_level  A string indicating the level of the file. Valid inputs are:
+#'   'household', 'h', 'person' or 'p'. If NULL (default), the file level will
+#'   be retrived from the 'lissy_files' attributes.
+#' @param variable_level Level of the variable. Should be either 'household', 'h', 'person' or 'p'.
+#'   If NULL (default), the function will try to guess the level of the variable.
+#'   This is done by comparing the value in 'variable' with pre-set lists of variables.
+#' @return A character vector with the name of the weight variable.
+determine_weight <- function(lissy_files, variable, files_level, variable_level){
+
+  level_ <- determine_file_level(lissy_files, files_level)
+
+  if(!is.null(level_) && level_ %in% c("household", "h")){
+    return("hwgt")
+  }
+
+  if(is.null(variable_level)){
+
+    variable_level <- check_variable_level(variable)
+
+  }else{
+
+    assertthat::assert_that(variable_level %in% c("person", "household", "p", "h"),
+                            msg = "Argument 'variable_level' can only take 'person', 'p', 'household' or 'h' as values.")
+  }
+
+  if(variable_level %in% c("person", "p")){
+
+    return("pwgt")
+
+  }else if(variable_level %in% c("household", "h")){
+
+    return("hwgt")
+
+  }
+
+}
+
+
+#' Determine the level of the file
+#'
+#' @param lissy_files A list of LIS or LWS files.
+#' @param files_level  A string indicating the level of the file. Valid inputs are:
+#'   'household', 'h', 'person' or 'p'. If NULL (default), the file level will
+#'   be retrived from the 'lissy_files' attributes.
+#'
+#' @return A character vector with the level of the file
+determine_file_level <- function(lissy_files, files_level){
+
+  if(missing(files_level)){
+
+    level_ <- get_lissy_attributes(lissy_files)[["level"]]
+
+  }else{
+
+    level_ <- files_level
+
+  }
+
+  return(level_)
+}
