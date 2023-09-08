134046a9bfe47e523ab07c24dd87f1d9f0ce6d07
JosepER <jespasareig@gmail.com>
2023-09-08 15:32:16 +0200

    add tests to read_lissy_files_locally

--- tests/testthat/test-read_lissy_files.R
+++ tests/testthat/test-read_lissy_files.R
@@ -53,5 +53,33 @@ test_that("read_lissy_files_locally outputs lists with the correct attributes",{
 
 
 test_that("read_lissy_files_locally throws error with invalid file names", {
+
   expect_error(read_lissy_files_locally(c("invalid.dta"), path_to_files = "testdata/"), "Argument 'files' should specify file names in 'ccyydl' or 'ccyyyydl' format.")
+
+})
+
+
+test_that("read_lissy_files_locally selects specified columns correctly", {
+
+  files_h <- suppressWarnings(read_lissy_files_locally(files = c("it14ih"), path_to_files = dir_testdata, col_select = c("hid", "dhi")))
+  files_p <- suppressWarnings(read_lissy_files_locally(files = c("it14ip"), path_to_files = dir_testdata, col_select = c("hid", "pi11")))
+
+  expect_equal(names(files_h[[1]]), c("hid", "dhi", "nhhmem", "hwgt", "hpopwgt", "hwgta"))
+  expect_equal(names(files_p[[1]]), c("hid", "pi11", "relation", "pwgt", "ppopwgt", "pwgta", "pid"))
+
+})
+
+
+test_that("read_lissy_files_locally returns names in 'ccyyyydl' format when full_year_names is TRUE", {
+
+  files_h <- suppressWarnings(read_lissy_files_locally(files = c("it14ih"), path_to_files = dir_testdata, full_year_names = TRUE))
+  expect_equal(names(files_h), "it2014ih")
+
+})
+
+test_that("read_lissy_files_locally returns names in 'ccyydl' format when full_year_names is FALSE", {
+
+  files_h <- suppressWarnings(read_lissy_files_locally(files = c("it14ih"), path_to_files = dir_testdata, full_year_names = FALSE))
+  expect_equal(names(files_h), "it14ih")
+
 })
