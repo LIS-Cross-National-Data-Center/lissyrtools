c38d772ae1d5a74e7bd93009f9c9e469c606eb2d
JosepER <jespasareig@gmail.com>
2023-09-08 15:49:14 +0200

    add more tests for read_lissy_files_locally using lws files

--- tests/testthat/test-read_lissy_files.R
+++ tests/testthat/test-read_lissy_files.R
@@ -22,30 +22,41 @@ test_that("read_lissy_files_locally merges LIS files correctly",{
 
 test_that("read_lissy_files_locally outputs lists with the correct attributes",{
 
-  files_h <- suppressWarnings(read_lissy_files_locally(files = c("it14ih", "us16ih"), path_to_files = dir_testdata))
-  files_p <- suppressWarnings(read_lissy_files_locally(files = c("it14ip", "us16ip"), path_to_files = dir_testdata))
+  files_ih <- suppressWarnings(read_lissy_files_locally(files = c("it14ih", "us16ih"), path_to_files = dir_testdata))
+  files_ip <- suppressWarnings(read_lissy_files_locally(files = c("it14ip", "us16ip"), path_to_files = dir_testdata))
 
-  expect_equal(attr(files_h,
+  files_wh <- suppressWarnings(read_lissy_files_locally(files = c("it14wh", "us16wh"), path_to_files = dir_testdata))
+  files_wp <- suppressWarnings(read_lissy_files_locally(files = c("it14wp", "us16wp"), path_to_files = dir_testdata))
+
+  expect_equal(attr(files_ih,
                     "merged_levels"),
                FALSE)
 
-  expect_equal(attr(files_p,
+  expect_equal(attr(files_ip,
                     "merged_levels"),
                FALSE)
 
-  expect_equal(attr(files_h,
+  expect_equal(attr(files_ih,
                     "database"),
                "i")
 
-  expect_equal(attr(files_p,
+  expect_equal(attr(files_ip,
                     "database"),
                "i")
 
-  expect_equal(attr(files_h,
+  expect_equal(attr(files_wh,
+                    "database"),
+               "w")
+
+  expect_equal(attr(files_wp,
+                    "database"),
+               "w")
+
+  expect_equal(attr(files_ih,
                     "level"),
                "h")
 
-  expect_equal(attr(files_p,
+  expect_equal(attr(files_ip,
                     "level"),
                "p")
 
@@ -61,11 +72,17 @@ test_that("read_lissy_files_locally throws error with invalid file names", {
 
 test_that("read_lissy_files_locally selects specified columns correctly", {
 
-  files_h <- suppressWarnings(read_lissy_files_locally(files = c("it14ih"), path_to_files = dir_testdata, col_select = c("hid", "dhi")))
-  files_p <- suppressWarnings(read_lissy_files_locally(files = c("it14ip"), path_to_files = dir_testdata, col_select = c("hid", "pi11")))
+  files_ih <- suppressWarnings(read_lissy_files_locally(files = c("it14ih"), path_to_files = dir_testdata, col_select = c("hid", "dhi")))
+  files_ip <- suppressWarnings(read_lissy_files_locally(files = c("it14ip"), path_to_files = dir_testdata, col_select = c("hid", "pi11")))
 
-  expect_equal(names(files_h[[1]]), c("hid", "dhi", "nhhmem", "hwgt", "hpopwgt", "hwgta"))
-  expect_equal(names(files_p[[1]]), c("hid", "pi11", "relation", "pwgt", "ppopwgt", "pwgta", "pid"))
+  files_wh <- suppressWarnings(read_lissy_files_locally(files = c("it14wh"), path_to_files = dir_testdata, col_select = c("hid", "dhi")))
+  files_wp <- suppressWarnings(read_lissy_files_locally(files = c("it14wp"), path_to_files = dir_testdata, col_select = c("hid", "pi11")))
+
+  expect_equal(names(files_ih[[1]]), c("hid", "dhi", "nhhmem", "hwgt", "hpopwgt", "hwgta"))
+  expect_equal(names(files_ip[[1]]), c("hid", "pi11", "relation", "pwgt", "ppopwgt", "pwgta", "pid"))
+
+  expect_equal(names(files_wh[[1]]), c("hid", "inum", "dhi", "nhhmem", "hwgt", "hpopwgt", "hwgta"))
+  expect_equal(names(files_wp[[1]]), c("hid", "inum" , "pid", "pi11", "relation", "pwgt", "ppopwgt", "pwgta"))
 
 })
 
