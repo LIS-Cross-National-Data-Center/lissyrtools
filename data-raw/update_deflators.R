d15714977336ab40999e43a91eb8fcafd8ca55a8
Gonçalo Marques <marques@lisproject.org>
2025-01-03 12:57:06 +0100

    changes in update_deflators.R + new gitignore

--- data-raw/update_deflators.R
+++ data-raw/update_deflators.R
@@ -2,6 +2,12 @@
 
 
 # create_ppp_files
-deflators <- tibble::as_tibble(read.delim("/media/includefiles/ppp_2017.txt", sep = ","))
-usethis::use_data(deflators, deflators, overwrite = TRUE)
 
+if(version$os ==  "linux-gnu"){
+  deflators <- tibble::as_tibble(read.delim("/media/includefiles/ppp_2017.txt", sep = ","))
+  usethis::use_data(deflators, deflators, overwrite = TRUE)
+
+} else {
+  deflators <- tibble::as_tibble(read.delim("S:/Upload/includefiles/ppp_2017.txt", sep = ","))
+  usethis::use_data(deflators, deflators, overwrite = TRUE)
+}
