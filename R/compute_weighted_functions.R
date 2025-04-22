e3bd427616cc811e71f56a340f05b8c318ec71d4
goncalomarques8 <goncalojosemarques@gmail.com>
2025-04-22 18:41:38 +0200

    added documentation on compute_weighted_functions

--- R/compute_weighted_functions.R
+++ R/compute_weighted_functions.R
@@ -1,11 +1,60 @@
 
 
+#' Compute Weighted Percentiles Across a List of Data Frames
+#' @description
+#' Applies the `compute_weighted_percentiles()` function to each data frame in a named list, using a specified variable and optional weight. 
+#' The function returns either percentile values or share values, depending on the `share` argument.
+#'
+#' @param data_list A named list of data frames, typically from multiple countries, years, or datasets.
+#' @param var_name A string specifying the variable name for which percentiles or shares should be computed.
+#' @param wgt_name An optional string specifying the weight variable to be used. If `NULL`, no weights are applied.
+#' @param probs A numeric vector of probabilities with values between 0 and 1. Used to determine percentile cutoffs.
+#' @param share Logical. If `TRUE`, returns income shares between percentile brackets instead of the percentile values.
+#' @param na.rm Logical. If `TRUE`, missing values in `var_name` or `wgt_name` are removed.
+#'
+#' @return A named list containing the result (numeric vectors) for each element of `data_list`, with names inherited from the list.
+#' 
+#' @details
+#' Percentiles are computed using the weighted version of **quantile definition 4** from Hyndman and Fan (1996). 
+#' When `share = TRUE`, the function estimates Lorenz ordinates by taking quantiles from the running sum of the ordered outcome variable (divided by the total), 
+#' according to the same quantile definition.
+#' 
+#' @export
+#'
+#' @examples
+#' \dontrun{  
+#' 
+#' # Import data
+#' my_data_list <- lissyuse(data = "es")
+#' 
+#' # Retrieve the percentile estimates
+#' percentiles_result <- my_data_list %>% 
+#'   transform_negative_values_to_zero("dhi") %>%
+#'   run_weighted_percentiles(
+#'     var_name = "dhi",
+#'     wgt_name = "hpopwgt",
+#'     probs = seq(0, 1, 0.1),
+#'     na.rm = TRUE
+#' )
+#' print(percentiles_result)
+#' 
+#' # Compute the distribution shares
+#' shares_result <- run_weighted_percentiles(
+#'     data_list = my_data_list,
+#'     var_name = "dhi",
+#'     wgt_name = "hpopwgt",
+#'     probs = seq(0, 1, 0.1),
+#'     share = TRUE,
+#'     na.rm = TRUE
+#' )
+#' print(shares_result)
+#' }
 run_weighted_percentiles <- function(data_list, var_name, wgt_name = NULL, probs = seq(0, 1, 0.25), share = FALSE, na.rm = TRUE) {
  
    data_list <- remove_dname_with_missings_in_weights(data_list, wgt_name) # return a list cleaned 
    check_input_in_weight_argument(wgt_name) 
   
-   output_run_weighted_percentiles <- imap(data_list, ~{
+   output_run_weighted_percentiles <- purrr::imap(data_list, ~{
     var <- .x[[var_name]]
     wgt <- if (!is.null(wgt_name)) .x[[wgt_name]] else NULL
     compute_weighted_percentiles(var = var, wgt = wgt, probs = probs, share = share, na.rm = na.rm)
@@ -18,18 +67,28 @@ run_weighted_percentiles <- function(data_list, var_name, wgt_name = NULL, probs
 
 
 
-#' Title
+#' Compute Weighted Percentiles or Share of Distribution
+#' 
+#' @description
+#' This function computes weighted percentiles of a numeric vector, or the share of total value within specified percentile intervals.
 #'
-#' @param var 
-#' @param wgt
-#' @param probs 
-#' @param na.rm 
-#' @param share 
 #'
-#' @return 
-#' @export
+#' @param x A numeric vector of values (e.g., .x$dhi, .x$pi11).
+#' @param w A numeric vector of weights (e.g., .x$hpopwgt, .x$pwgt). Must be the same length as \code{x}.
+#' @param probs A numeric vector of probabilities between 0 and 1 indicating which percentiles to compute. Default is \code{seq(0, 1, 0.25)}.
+#' @param na.rm Logical; if \code{TRUE}, missing values in \code{x} and \code{w} are removed before computation. Default is \code{TRUE}.
+#' @param share Logical; if \code{TRUE}, computes the share of total value (e.g., .x$dhi) within each interval defined by \code{probs}. If \code{FALSE}, returns the percentile values. Default is \code{FALSE}.
+#'
+#'
+#' @return A named numeric vector. If \code{share = FALSE}, returns weighted percentiles with names corresponding to the percentiles (e.g., "25%"). If \code{share = TRUE}, returns the share of the total value in each percentile range (e.g., "0-25%"). 
+#' @keywords internal
 #'
 #' @examples
+#' \dontrun{
+#' compute_weighted_percentiles(data$es22$dhi, data$es22$ppopwgt)
+#' compute_weighted_percentiles(data$es22$dhi, data$es22$ppopwgt, probs = c(0.03, 0.72, 0.48, .01))
+#' compute_weighted_percentiles(data$es22$dhi, data$es22$ppopwgt, probs = c(0.03, 0.72, 0.48, .01), share = TRUE)
+#' }
 compute_weighted_percentiles <- function(var, wgt = NULL, probs = seq(0, 1, 0.25), na.rm = TRUE, share = FALSE) {
   
   
