e58532e094d7bcb742971d924bb32db56053bca7
Gonçalo Marques <marques@lisproject.org>
2025-07-30 16:23:48 +0200

    check_github_version

--- R/utils.R
+++ R/utils.R
@@ -724,3 +724,34 @@ check_missing_share <- function(data_list, include_zeros = FALSE) {
     )
   })
 }
+
+
+#' Retrieve the current version of lissyrtools from the GitHub repository
+#'
+#' @description
+#' Fetches the latest version of the `lissyrtools` package from its DESCRIPTION file on GitHub.
+#' This allows comparison with the locally installed version. Using an outdated version may
+#' result in missing support for the most recent countries, datasets, or variable revisions,
+#' which can cause inaccurate outputs in functions that rely on up-to-date metadata.
+#'
+#' @return A character string indicating the current version on GitHub.
+#' @export
+#' @examples
+#' \dontrun{
+#' library(lissyrtools)
+#' library(utils)
+#' library(assertthat)
+#' 
+#' local_version <- as.character(utils::packageVersion("lissyrtools"))
+#' remote_github_version <- check_github_version()
+#' 
+#' assertthat::assert_that(local_version == remote_github_version)
+#' }
+check_github_version <- function() {
+  url <- "https://raw.githubusercontent.com/LIS-Cross-National-Data-Center/lissyrtools/main/DESCRIPTION"
+  desc_lines <- readLines(url, warn = FALSE)
+  version_line <- grep("^Version:", desc_lines, value = TRUE)
+  version <- sub("Version:\\s*", "", version_line)
+  return(version)
+}
+
